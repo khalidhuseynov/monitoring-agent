c5b3dbba86d96ea4e75ff912d1721f3852536b37
anthony corbacho <corbacho.anthony@gmail.com>
2014-07-14 15:39:39 +0900

    update tests

--- test/nflabs-status-agent/firstTest.sh
+++ test/nflabs-status-agent/firstTest.sh
@@ -13,9 +13,7 @@ function test_get_memory_usage_works() {
 
 function test_get_load_average_works() {
   assertNotNull $(get_load_average)
-  local value
-  value="\"load_1m\":, "\"load_5m\":, "\"load_15m\":, \"cpu_usage\""
-  assertNotEquals $(get_ip_address) ${value}
+  assertNotEquals $(get_ip_address) " "
 }
 
 function test_get_disk_usage_works() {
@@ -25,9 +23,7 @@ function test_get_disk_usage_works() {
 
 function test_get_network_usage_works() {
   assertNotNull $(get_network_usage)
-  local value
-  value="\"net_receive\": ,\"net_send\":"
-  assertNotEquals $(get_ip_address) ${value}
+  assertNotEquals $(get_ip_address) " "
 }
 
 function test_get_ip_address_works() {
