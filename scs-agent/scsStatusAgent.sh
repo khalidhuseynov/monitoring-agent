e74385d4d237715ffc48a6ccf5a50e6ce8178361
anthony corbacho <corbacho.anthony@gmail.com>
2014-07-14 12:14:11 +0900

    update script

--- scs-agent/scsStatusAgent.sh
+++ scs-agent/scsStatusAgent.sh
@@ -5,29 +5,22 @@
 # Include all functions
 . function
 
+readonly ELASTICSEARCH_NODE="http://apple.nflabs.com:9200/"
+readonly ELASTICSEARCH_INDICE="nflabsmonitor-"
+readonly ELASTICSEARCH_INDEX="nflabsmonitor"
+
 readonly DISK_USAGE=$(get_disk_usage)
 readonly MEMORY_USAGE=$(get_memory_usage)
 readonly LOAD_AVERAGE=$(get_load_average)
 readonly NETWORK_USAGE=$(get_network_usage)
 readonly IP_ADRESSES=$(get_ip_address)
 readonly HOSTANME=$(hostname)
-
-echo $DISK_USAGE
-#echo $netConn
-#echo $memUsage
-#echo $netUsage
-#echo $loadAvg
-
-json="{\"disk_usage\":$diskUsage,\"ipaddr\":$ipAddress,\"region\":\"KR\",$netUsage,$memUsage,$loadAvg,\"host\":\"$hostname\",\"@timestamp\":$(($(date +%s%N)/1000000))}" 
-
-#echo $json
-#exit 0
-# curl --connect-timeout 3 --request POST -T "${line}" "${URL}" | grep OK
-
+readonly JSON="{\"disk_usage\":$diskUsage,\"ipaddr\":$ipAddress,\"region\":\"KR\",$netUsage,$memUsage,$loadAvg,\"host\":\"$hostname\",\"@timestamp\":$(($(date +%s%N)/1000000))}" 
 readonly TODAY=$(date -d "today" +"%Y.%m.%d")
-# curl -XPOST newmonitor.nfractals.com:9200/scsstat-2014.04.11/scsstat -d '$json'
 
-#curl -s --connect-timeout 3 -XPOST "http://newmonitor.nfractals.com:9200/scsmonitor-$TODAY/scsmonitor" -d "$json" | grep "\"created\":true"
+# Add node status to ES
+
+#curl -s --connect-timeout 3 -XPOST "${ELASTICSEARCH_NODE}/${ELASTICSEARCH_INDICE}-$TODAY/${ELASTICSEARCH_INDEX}" -d "${JSON}" | grep "\"created\":true"
 #if [ $? -ne 0 ]; then
 #	log "send system status failed. $json"
 #else
