121c62589c4daf5d25c52629442d08440f9a6f65
anthonyc <anthonyc@mango.nflabs.com>
2014-07-14 14:34:49 +0900

    update script

--- nflabs-status-agent/nflabs_status_agent.sh
+++ nflabs-status-agent/nflabs_status_agent.sh
@@ -5,7 +5,7 @@
 # Include all functions
 . functions
 
-readonly ELASTICSEARCH_NODE="http://apple.nflabs.com:9200/"
+readonly ELASTICSEARCH_NODE="http://apple.nflabs.com:9200"
 readonly ELASTICSEARCH_INDICE="nflabsmonitor-"
 readonly ELASTICSEARCH_INDEX="nflabsmonitor"
 
@@ -15,15 +15,14 @@ readonly LOAD_AVERAGE=$(get_load_average)
 readonly NETWORK_USAGE=$(get_network_usage)
 readonly IP_ADRESSES=$(get_ip_address)
 readonly HOSTANME=$(hostname)
-readonly JSON="{\"disk_usage\":$diskUsage,\"ipaddr\":$ipAddress,\"region\":\"KR\",$netUsage,$memUsage,$loadAvg,\"host\":\"$hostname\",\"@timestamp\":$(($(date +%s%N)/1000000))}" 
+readonly JSON="{\"disk_usage\":${DISK_USAGE},\"ipaddr\":${IP_ADRESSES},\"region\":\"KR\",${NETWORK_USAGE},${MEMORY_USAGE},${LOAD_AVERAGE},\"host\":\"${HOSTANME}\",\"@timestamp\":$(($(date +%s%N)/1000000))}" 
 readonly TODAY=$(date -d "today" +"%Y.%m.%d")
 
 # Add node status to ES
-
-#curl -s --connect-timeout 3 -XPOST "${ELASTICSEARCH_NODE}/${ELASTICSEARCH_INDICE}-$TODAY/${ELASTICSEARCH_INDEX}" -d "${JSON}" | grep "\"created\":true"
-#if [ $? -ne 0 ]; then
-#	log "send system status failed. $json"
-#else
-#	echo "success"
-#fi
+curl -s --connect-timeout 3 -XPOST "${ELASTICSEARCH_NODE}/${ELASTICSEARCH_INDICE}${TODAY}/${ELASTICSEARCH_INDEX}" -d "${JSON}" | grep "\"created\":true"
+if [ $? -ne 0 ]; then
+  logger "NFLabs monitor : send system status failed."
+else
+  logger "NFLabs monitor : send system status OKAY."
+fi
 
