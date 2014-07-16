d5bf951d8a970f709bd51aa9c5c12e8a09c2de1a
anthony corbacho <corbacho.anthony@gmail.com>
2014-07-16 12:05:41 +0900

    add service check

--- nflabs-status-agent/nflabs_status_agent.sh
+++ nflabs-status-agent/nflabs_status_agent.sh
@@ -10,12 +10,19 @@ readonly ELASTICSEARCH_NODE="http://apple.nflabs.com:9200"
 readonly ELASTICSEARCH_INDICE="nflabsmonitor-"
 readonly ELASTICSEARCH_INDEX="nflabsmonitor"
 
+# Service to check per node
+readonly SERVICES_FILE="./services"
+
 readonly DISK_USAGE=$(get_disk_usage)
 readonly MEMORY_USAGE=$(get_memory_usage)
 readonly LOAD_AVERAGE=$(get_load_average)
 readonly NETWORK_USAGE=$(get_network_usage)
 readonly IP_ADRESSES=$(get_ip_address)
 readonly HOSTANME=$(hostname)
+readonly SERVICE=$(get_services_running ${SERVICES_FILE})
+
+echo ${SERVICE}
+
 readonly JSON="{\"disk_usage\":${DISK_USAGE},\"ipaddr\":${IP_ADRESSES},\"region\":\"KR\",${NETWORK_USAGE},${MEMORY_USAGE},${LOAD_AVERAGE},\"host\":\"${HOSTANME}\",\"@timestamp\":$(($(date +%s%N)/1000000))}" 
 readonly TODAY=$(date -d "today" +"%Y.%m.%d")
 
@@ -26,4 +33,3 @@ if [ $? -ne 0 ]; then
 else
   logger "NFLabs monitor : send system status OKAY."
 fi
-
